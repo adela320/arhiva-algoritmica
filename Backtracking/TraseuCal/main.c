2037195eb99cef7b455cf5a9acfa268be3b8c063
Adela44 <adacodrean@gmail.com>
2026-01-06 13:31:39 +0200

    afisare primei solutii a tablei

--- Backtracking/TraseuCal/main.c
+++ Backtracking/TraseuCal/main.c
@@ -5,12 +5,26 @@ const int dx[] = {-2,-2,-1,-1,1, 1,2, 2};
 const int dy[] = {1, -1, 2,-2,2,-2,1,-1};
 int b[10][10];
 int cnt = 0;
+
+void afisare_sol(int n, int m) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            printf("%d ", b[i][j]);
+        }
+        printf("\n");
+    }
+}
+
 void Bk(int a[10][10], int n, int m, int i, int j, int pas) {
     if (i < 0 || j < 0 || i >= n || j >= m || a[i][j] != 0 || b[i][j] != 0) {
         return;
     }
     if (i == n-1 && j == m-1) {
         cnt++;
+        b[i][j] = pas;
+        if (cnt == 1) {
+            afisare_sol(n, m);
+        }
     }
     else {
         b[i][j] = pas;
